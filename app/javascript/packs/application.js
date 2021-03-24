c152622fe8e9b5612719c7da4ea1d42f05344d60
Brian Von De Khan <laststraw10@gmail.com>
2021-03-24 12:13:40 +0000

    New2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,17 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("rails/activestorage").start()
-require("channels")
-
-
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-import "bootstrap"
