52a05e15700bce23fec60442ef5913816443a313
Brian Von De Khan <laststraw10@gmail.com>
2021-03-24 09:37:02 +0000

    js file removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,3 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-<script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
-<script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
